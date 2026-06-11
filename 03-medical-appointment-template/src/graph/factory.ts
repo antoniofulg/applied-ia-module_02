4103fb753fbf847805a46367011db1f28846bddf
Antonio Fulgencio <antoniofulg@gmail.com>
2026-06-11 14:08:04 -0300

    Turning user intention in json

--- 03-medical-appointment-template/src/graph/factory.ts
+++ 03-medical-appointment-template/src/graph/factory.ts
@@ -1,11 +1,16 @@
-import { buildAppointmentGraph } from "./graph.ts";
+import { config } from "../config.ts"
+import { AppointmentService } from "../services/appointmentService.ts"
+import { OpenRouterService } from "../services/openRouterService.ts"
+import { buildAppointmentGraph } from "./graph.ts"
 
 export function buildGraph() {
-  return buildAppointmentGraph();
+	const llmClient = new OpenRouterService(config)
+	const appointmentService = new AppointmentService()
+	return buildAppointmentGraph(llmClient, appointmentService)
 }
 
 export const graph = async () => {
-  return buildGraph();
-};
+	return buildGraph()
+}
 
-export default graph;
+export default graph
