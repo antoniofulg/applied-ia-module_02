65d7b105b58d4d1e76897c6f50c54481ae614a5f
Antonio Fulgencio <antoniofulg@gmail.com>
2026-06-18 14:52:30 -0300

    Add question complexity analyzer

--- 06-rag-neo4j-students/src/index.ts
+++ 06-rag-neo4j-students/src/index.ts
@@ -1,9 +1,9 @@
-import { createServer } from './server.ts';
+import { createServer } from "./server.ts"
 
-const app = createServer();
+const app = createServer()
 
-await app.listen({ port: 4000, host: '0.0.0.0' });
-console.log(`Server is running on http://0.0.0.0:4000`);
+await app.listen({ port: 4000, host: "0.0.0.0" })
+console.log(`Server is running on http://0.0.0.0:4000`)
 
 //  curl \
 //  -X POST \
@@ -11,17 +11,21 @@ console.log(`Server is running on http://0.0.0.0:4000`);
 //  --data '{"question": "upper"}' \
 //  localhost:4000/chat
 
-app.inject({
-    method: 'POST',
-    url: '/sales',
-    payload: {
-        question: 'Which courses are commonly bought together?', // complex
-        // question: "Find courses that students typically purchase after 'Machine Learning em Navegadores'", // complex
-        // question: 'Show me the revenue distribution across all courses',
-        // question: 'Which users have progressed over 80%?',
-    },
-}).then(response => {
-    console.log(JSON.parse(response.body)?.answer);
-}).catch(error => {
-    console.error('Error making test request:', error);
-})
+app
+	.inject({
+		method: "POST",
+		url: "/sales",
+		payload: {
+			question: "Which courses are commonly bought together?", // complex
+			// question: "Find courses that students typically purchase after 'Machine Learning em Navegadores'", // complex
+			// question: 'Show me the revenue distribution across all courses',
+			// question: 'Which users have progressed over 80%?',
+			// question: "How many courses does the Academy have?", // simpler
+		},
+	})
+	.then((response) => {
+		console.log(JSON.parse(response.body)?.answer)
+	})
+	.catch((error) => {
+		console.error("Error making test request:", error)
+	})
